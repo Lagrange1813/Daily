36c681c20a430c69d83dd4294f61c1a6137abe66
WeiXi Zhang <54029053+Lagrange1813@users.noreply.github.com>
2022-07-26 00:53:29 +0800

    fix using http connection

--- Daily/Daily/ViewController.swift
+++ Daily/Daily/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		view.backgroundColor = .blue
+		
+		let test = ArticleService()
+		test.test()
 	}
 
 
