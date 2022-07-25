16673bd6ad4f88ef1844a9265c23d4f7449c9174
Lagrange1813 <Lagrange1813@163.com>
2022-07-25 21:02:52 +0800

    init project.

---
+++ Daily/Daily/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daily
+//
+//  Created by 张维熙 on 2022/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
