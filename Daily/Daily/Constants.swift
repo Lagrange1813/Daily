e5e61d50c6e5b08b553c85742c287574a6a5774d
WeiXi Zhang <54029053+Lagrange1813@users.noreply.github.com>
2022-07-27 15:41:24 +0800

    ♻️ Refactor.

---
+++ Daily/Daily/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Daily
+//
+//  Created by 张维熙 on 2022/7/27.
+//
+
+import UIKit
+
+enum Constants {
+	static let bottomInset = UIApplication.shared.windows.first?.safeAreaInsets.bottom ?? 0
+	static let width = UIScreen.main.bounds.width
+}
